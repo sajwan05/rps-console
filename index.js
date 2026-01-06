5f81be81c1a0f9e5e2932e081e7613ac42c0521e
sajwan05 <surajsajwan328@gmail.com>
2026-01-06 22:10:07 +0530

    Make playRound so you can play one round

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 console.log("Javascript is connected successfully!");
 
+let humanScore = 0;
+let computerScore = 0;
 function getComputerChoice() {
     const choices = ["Rock", "Paper", "Scissors"];
 
@@ -8,10 +10,9 @@ function getComputerChoice() {
     return choices[index].toLowerCase();
 }
 
-getComputerChoice();
 
 function getHumanChoice() {
-    const choice = prompt("Rock Paper scissors shoot....").toLowerCase();
+    // const choice = prompt("Rock Paper scissors shoot....").toLowerCase();
 
     if (choice === "rock" || choice === "scissors" || choice === "paper"){
         console.log(choice);
@@ -21,4 +22,25 @@ function getHumanChoice() {
     }
 }
 
-getHumanChoice();
+let humanChoice = getHumanChoice();
+let computerChoice = getComputerChoice();
+
+let res = playRound(humanChoice, computerChoice);
+
+function playRound(userChoice, botChoice) {
+    if ((userChoice === "rock" && botChoice === "scissors") || (userChoice === "scissors" && botChoice === "rock") || (userChoice === "paper" && botChoice === "rock") ){
+        humanScore++;
+        console.log("You Won this round");
+    }else if(userChoice === botChoice) {
+        humanScore++;
+        computerScore++;
+        console.log("This round is Tied!")
+    }else {
+        computerScore++;
+        console.log("You lost");
+    }
+}
+
+function playGame() {
+    
+}
